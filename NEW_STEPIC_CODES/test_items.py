3e1a5bb5e22e4948cb674f65bd9d2464482daf83
MalofeevDenis <dmalofeev@script-master.net>
2026-06-26 14:54:49 +0300

    Corrected script for step 3

--- NEW_STEPIC_CODES/test_items.py
+++ NEW_STEPIC_CODES/test_items.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,6 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_can_add_product_to_basket(browser):
+
+    browser = webdriver.Chrome()
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+
     """
     Тест проверяет возможность добавления товара в корзину.
     1. Открывает страницу товара.
